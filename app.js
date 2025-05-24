fd9aef5e93f1b2257b2b14b08d83a8bee10beede
Soujanya Maharudra Bailawad <soujanyabailawad@gmail.com>
2025-05-24 19:21:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 //creating server and serving html and css files
 //both index.html and style.css files must be placed in another folder "public"
+
 import { readFile } from "fs/promises";
 import {createServer} from "http";
+import crypto from "crypto";
 import path from "path";
+import { writeFile } from "fs/promises";
 
 const PORT = 3000;
+const DATA_FILE = path.join("data", 'links.json');
 
 const serveFile = async (res, filePath, contentType) => {
      try{
@@ -12,12 +16,30 @@ const serveFile = async (res, filePath, contentType) => {
                  res.writeHead(200,{'Content-Type': contentType});
                  res.end(data);
             }catch(error){
-                res.writeHead(404,{'Content-Type':"content/plain"});
+                res.writeHead(404,{'Content-Type':"text/plain"});
                 res.end("404 page not found")
             }
 }
 
-const server = createServer((req,res)=> {
+const loadLinks = async () => {
+    try{
+      const data = await readFile(DATA_FILE, "utf-8");
+      return JSON.parse(data);
+
+
+    }catch(error){
+        if(error.code === "ENOENT"){
+            await writeFile(DATA_FILE, JSON.stringify({}));
+            return {};
+        }
+        throw error;
+    }
+}
+
+const saveLinks = async (links)=>{
+    await writeFile(DATA_FILE, JSON.stringify(links))
+}
+const server = createServer(async (req,res)=> {
     console.log(req.url)
     if(req.method === "GET"){
         if(req.url === "/"){
@@ -25,8 +47,8 @@ const server = createServer((req,res)=> {
           
 
 }
-    }else if(req.method === "GET"){
-        if(req.url === "style.css"){
+
+        else if(req.url === "/style.css"){
            return serveFile(
             res,
             path.join("public","style.css"),   "text/css")
@@ -35,7 +57,42 @@ const server = createServer((req,res)=> {
         }
     }
 
-})
+    if(req.method === "POST" && req.url === "/shorten"){
+
+        const links = await loadLinks();
+        let  body ="";
+        req.on("data",(chunk)=> body += chunk
+
+        )
+        req.on('end',async () =>{
+            console.log(body);
+            const {url,shorCode} = JSON.parse(body)
+
+            if(!url){
+                res.writeHead(404,{"Content-Type":"text/plain"});
+                return res.end("URL IS REQUIRED")
+            }
+
+
+            const finalShortCode = shorCode || crypto.randomBytes(4).toString("hex");
+            if(links[finalShortCode]){
+                res.writeHead(404,{"Content-Type":"text/plain"})
+               return res.end("short code already exists give another one") 
+            }
+
+            links[finalShortCode] = url;
+
+
+
+            await saveLinks(links);
+            res.writeHead(200,{"Content-Type":"plain/text"});
+            res.end(JSON.stringify({success:true, shortCode:finalShortCode}));
+        })
+    }
+
+    }
+
+);
 
 server.listen(PORT,()=>{
     console.log(`server running at http://localhost:${PORT}`)
